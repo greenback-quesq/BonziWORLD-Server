4f0227413a29ae9cbbaa4aa067cb3ddef95b77ab
Greenback <86879726+greenback-quesq@users.noreply.github.com>
2026-09-23 17:43:27 +0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -67,14 +67,6 @@ function linkify(a) {
 }
 function loadBonzis(a) {
     loadQueue.loadManifest([
-        { id: "bonziBlack", src: "./img/bonzi/black.png" },
-        { id: "bonziBlue", src: "./img/bonzi/blue.png" },
-        { id: "bonziBrown", src: "./img/bonzi/brown.png" },
-        { id: "bonziGreen", src: "./img/bonzi/green.png" },
-        { id: "bonziPurple", src: "./img/bonzi/purple.png" },
-        { id: "bonziRed", src: "./img/bonzi/red.png" },
-        { id: "bonziPink", src: "./img/bonzi/pink.png" },
-        { id: "topjej", src: "./img/misc/topjej.png" },
     ]),
         loadQueue.on(
             "fileload",
